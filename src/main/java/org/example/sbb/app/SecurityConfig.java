5d5b35f9688dc5c10a38db412693920f7ee1ae53
omegafrog <jiwoo22110103@gmail.com>
2025-01-13 12:10:41 +0900

    chore: h2 database 설정

--- src/main/java/org/example/sbb/app/SecurityConfig.java
+++ src/main/java/org/example/sbb/app/SecurityConfig.java
@@ -1,22 +1,34 @@
-package org.example.sbb.app.common;
+package org.example.sbb.app;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+//    @Bean
+//    public SecurityFilterChain global(HttpSecurity http) throws Exception {
+//        http.
+//                securityMatcher("/**")
+//                .authorizeHttpRequests(authorizeRequests ->
+//                        authorizeRequests.requestMatchers("/**")
+//                                .permitAll())
+//                .formLogin(AbstractHttpConfigurer::disable);
+//        return http.build();
+//    }
 
     @Bean
-    SecurityFilterChain global(HttpSecurity http) throws Exception {
-        http.
-                securityMatcher("/**")
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/**")
-                                .permitAll());
-        return http.build();
+    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
     }
 }
