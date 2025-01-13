beaa27e0ee7b60e1fcd388f5da8947ed85a8f490
omegafrog <jiwoo22110103@gmail.com>
2025-01-13 11:11:07 +0900

    feat: hello world controller

---
+++ src/main/java/org/example/sbb/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.example.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/sbb")
+public class HelloController {
+
+    @GetMapping
+    @ResponseBody
+    public String hello(){
+        return "안녕하세요 sbb에 오신 것을 환영합니다.";
+    }
+}
